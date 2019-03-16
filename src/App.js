bb06c1c29772852880c551155f6d2d4038af443b
StringEpsilon <StringEpsilon@gmail.com>
2019-03-16 09:36:28 +0100

    Made replication for react-router bug.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Redirect, Link, Route, Switch } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+		<Link to="/">Home</Link>
+    );
+  }
+}
+
+export default App;
